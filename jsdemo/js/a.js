06bbee0eef399aab3953c69188faab994e231f93
loooha <ryuje1029@gmail.com>
2022-09-19 15:56:20 +0900

    20220919

---
+++ jsdemo/js/a.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+  console.log("Hello");
+  let data = document.getElementById("wrap");
+  console.log(data.innerText);
+};
+
+//옛날 방식
+// <script type="text/javascript" src="js/a.js"></script>
+
+//  $(document).ready(function(){});
